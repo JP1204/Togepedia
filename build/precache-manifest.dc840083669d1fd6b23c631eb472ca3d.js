fd7963a9ef501498196ac5a1b73a17f924292c32
Jimmy Phan <jimmyphan777@gmail.com>
2020-09-18 15:05:44 -0500

    deploying

---
+++ build/precache-manifest.dc840083669d1fd6b23c631eb472ca3d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e813f27fb30e87fc970f1c22fe8c05c8",
+    "url": "/Togepedia/index.html"
+  },
+  {
+    "revision": "071cd0d138fb0b49f4e7",
+    "url": "/Togepedia/static/css/2.2d63d0b4.chunk.css"
+  },
+  {
+    "revision": "57ba4e45434287264dd7",
+    "url": "/Togepedia/static/css/main.5d01dc87.chunk.css"
+  },
+  {
+    "revision": "071cd0d138fb0b49f4e7",
+    "url": "/Togepedia/static/js/2.6d2a2a6f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Togepedia/static/js/2.6d2a2a6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ba4e45434287264dd7",
+    "url": "/Togepedia/static/js/main.e65cf191.chunk.js"
+  },
+  {
+    "revision": "d472be261da62bf39b28",
+    "url": "/Togepedia/static/js/runtime-main.1f885b47.js"
+  },
+  {
+    "revision": "fe22dad652130a6b2a567fce4b67e7de",
+    "url": "/Togepedia/static/media/BlankPokemon.fe22dad6.png"
+  },
+  {
+    "revision": "d41c5002d076258eee908cfb0f53dc44",
+    "url": "/Togepedia/static/media/Types Chart.d41c5002.jpg"
+  },
+  {
+    "revision": "25e309404b737bf64e618472c3a8480a",
+    "url": "/Togepedia/static/media/bulbasaur.25e30940.png"
+  },
+  {
+    "revision": "56053cacbc3d8938bc620049ae209b82",
+    "url": "/Togepedia/static/media/bulbasaur_background.56053cac.jpg"
+  },
+  {
+    "revision": "22093aeb965dfb2b5c8a63f4b4c484ca",
+    "url": "/Togepedia/static/media/eevee.22093aeb.png"
+  },
+  {
+    "revision": "3186a2f1f98f3c6fa36c66f948eebc1e",
+    "url": "/Togepedia/static/media/eevee_background.3186a2f1.jpg"
+  },
+  {
+    "revision": "72fc25adc5863ce0b3d8ce45f8a9eedb",
+    "url": "/Togepedia/static/media/jaime.72fc25ad.jpg"
+  },
+  {
+    "revision": "a73004cb953d1cc4a24365857be1ccbe",
+    "url": "/Togepedia/static/media/jimmy.a73004cb.jpg"
+  },
+  {
+    "revision": "cc65d30511097505a1ce8a341ab4f776",
+    "url": "/Togepedia/static/media/mihir.cc65d305.jpg"
+  },
+  {
+    "revision": "46e57181673f182147d6423bd1b12d49",
+    "url": "/Togepedia/static/media/pikachu.46e57181.png"
+  },
+  {
+    "revision": "3df20d4cad276981cf87bd4f53abce9c",
+    "url": "/Togepedia/static/media/pikachu_background.3df20d4c.png"
+  },
+  {
+    "revision": "7937b55216a7202bbc0dfda666be53ec",
+    "url": "/Togepedia/static/media/togepi.7937b552.png"
+  },
+  {
+    "revision": "46f97f6e954ae8df1397aef233eccfef",
+    "url": "/Togepedia/static/media/togepi_background.46f97f6e.jpg"
+  },
+  {
+    "revision": "7f36044a6ed952e2798703cdfb7b56e4",
+    "url": "/Togepedia/static/media/tristan.7f36044a.jpeg"
+  }
+]);
